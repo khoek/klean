0b4740b9eb6ee971c47e0d6b740436f96376f3a3
Keeley Hoek <keeley@hoek.io>
2018-08-01 01:56:44 +1000

    Fix wrong author information

--- src/library/vm/lib_decent.h
+++ src/library/vm/lib_decent.h
@@ -1,3 +1,9 @@
+/*
+Copyright (c) 2018 Keeley Hoek. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+
+Author: Keeley Hoek
+*/
 #pragma once
 
 #include <cstdint>
--- src/library/vm/vm_extras.h
+++ src/library/vm/vm_extras.h
@@ -1,8 +1,8 @@
 /*
-Copyright (c) 2016 Microsoft Corporation. All rights reserved.
+Copyright (c) 2018 Keeley Hoek. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 
-Author: Leonardo de Moura
+Author: Keeley Hoek
 */
 #pragma once
 #include <string>
