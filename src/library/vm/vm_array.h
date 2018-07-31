9b0679c457e0e0428b8d4cd181a4cb7329addffb
Keeley Hoek <keeley@hoek.io>
2018-08-01 01:06:23 +1000

    Add list_to_array() to vm/vm_array.h

--- src/library/vm/vm_array.h
+++ src/library/vm/vm_array.h
@@ -8,11 +8,16 @@ Author: Leonardo de Moura
 
 #include "library/parray.h"
 #include "library/vm/vm.h"
+#include "library/vm/vm_list.h"
 
 namespace lean {
 
 parray<vm_obj> const & to_array(vm_obj const & o);
 vm_obj to_obj(parray<vm_obj> const & a);
+
+static __attribute__((unused)) list<parray<vm_obj>> to_list_array(vm_obj const & obj) {
+    return to_list<parray<vm_obj>>(obj, [](vm_obj const & o) { return to_array(o); });
+}
 
 void initialize_vm_array();
 void finalize_vm_array();
